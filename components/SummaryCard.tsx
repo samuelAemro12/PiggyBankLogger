dd4622abbc041bfdc71e4edf559949143be1ddf6
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-08-29 20:41:37 +0300

    Feat: Summary display made

---
+++ components/SummaryCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const SummaryCard = () => (
+  <View>
+    <Text>Total Saved: $120.00</Text>
+    <Text>This Month: $40.00</Text>
+  </View>
+);
+
+export default SummaryCard;
