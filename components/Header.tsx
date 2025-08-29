8f299dcc5b2426de483fccb9684f6098a6146f71
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-08-29 20:40:30 +0300

    Feat: Made Header

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => (
+  <View>
+    <Text>PiggyBank Logger (avatar)</Text>
+  </View>
+);
+
+export default Header;
