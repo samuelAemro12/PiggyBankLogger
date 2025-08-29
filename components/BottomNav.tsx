8879112204de3f56e7d758bbd46de4fc01858fd3
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-08-29 20:39:45 +0300

    Feat: bottom nav

---
+++ components/BottomNav.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const BottomNav = () => (
+  <View>
+    <Text>Bottom Nav: Home | Stats | Settings</Text>
+  </View>
+);
+
+export default BottomNav;
