59dba4f2831945a02c39c53ca0a89495b17f7ef5
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-08-30 00:12:56 +0300

    Feat: Mounted the BalanceTable

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ScrollView, View } from 'react-native';
+
 import ActionButtons from '../components/ActionButtons';
 import BottomNav from '../components/BottomNav';
 import Header from '../components/Header';
 import RecentActivityItem from '../components/RecentActivityItem';
 import SummaryCard from '../components/SummaryCard';
+import BalanceTable from '../components/BalanceTable';
 
 const HomeScreen = () => (
   <View className="relative flex-1 bg-background">
@@ -14,7 +16,8 @@ const HomeScreen = () => (
       <ActionButtons />
       <RecentActivityItem type="+" amount="20" label="Salary" date="Aug 27" />
       <RecentActivityItem type="-" amount="5" label="Coffee" date="Aug 26" />
-      <RecentActivityItem type="+" amount="10" label="Gift" date="Aug 25" />
+  <RecentActivityItem type="+" amount="10" label="Gift" date="Aug 25" />
+  <BalanceTable />
     </ScrollView>
     <BottomNav />
   </View>
