b59bc48ec815a2619b7f1e0325119c1f5244b087
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-08-29 20:42:31 +0300

    Feat: made screens placeholder

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View } from 'react-native';
+import ActionButtons from '../components/ActionButtons';
+import BottomNav from '../components/BottomNav';
+import Header from '../components/Header';
+import RecentActivityItem from '../components/RecentActivityItem';
+import SummaryCard from '../components/SummaryCard';
+
+const HomeScreen = () => (
+  <View>
+    <Header />
+    <SummaryCard />
+    <ActionButtons />
+    <RecentActivityItem type="+" amount="20" label="Salary" date="Aug 27" />
+    <RecentActivityItem type="-" amount="5" label="Coffee" date="Aug 26" />
+    <RecentActivityItem type="+" amount="10" label="Gift" date="Aug 25" />
+    <BottomNav />
+  </View>
+);
+
+export default HomeScreen;
---
+++ screens/SettingsScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const SettingsScreen = () => (
+  <View>
+    <Text>Settings Screen (placeholder)</Text>
+  </View>
+);
+
+export default SettingsScreen;
