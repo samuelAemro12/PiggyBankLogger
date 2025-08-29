314d642e3c3d061a79aa42780347c57c5d56b45d
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-08-29 20:39:06 +0300

    Feat: made button

---
+++ components/ActionButtons.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button, View } from 'react-native';
+
+const ActionButtons = () => (
+  <View>
+    <Button title="+ Add Deposit" onPress={() => {}} />
+    <Button title="- Add Expense" onPress={() => {}} />
+  </View>
+);
+
+export default ActionButtons;
